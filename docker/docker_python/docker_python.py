83f0253257199fef14aa6c64182324f1de32a669
netrunnercl <rafael.torrales@gmail.com>
2021-07-05 00:08:47 +0200

    Delete docker directory

--- docker/docker_python/docker_python.py
+++
@@ -1,6 +0,0 @@
-from skilletlib.skilletLoader import SkilletLoader
-
-sl = SkilletLoader()
-skillet = sl.load_skillet_from_path('.')
-out = skillet.execute(dict())
-print(out)
--- docker/docker_shell/example.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-SCRIPT_ARG=$1
-
-echo "This is an example shell script!"
-
-echo "Your script argument was ${SCRIPT_ARG}"
-
-exit 0
